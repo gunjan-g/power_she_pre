aaadb88cb1e33311cdb4480d3f9a460209f94d10
Nancy <you@example.com>
2023-01-07 12:52:02 +0530

    Opportunities

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:power_she_pre/screens/carousel.dart';
 import 'package:power_she_pre/screens/course.dart';
 import 'package:power_she_pre/screens/home_screen.dart';
 import 'package:power_she_pre/screens/splash.dart';
 import 'package:power_she_pre/screens/welcome_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:power_she_pre/screens/carousel.dart';
+import 'package:power_she_pre/screens/opr_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,13 +28,14 @@ class MyApp extends StatelessWidget {
           Theme.of(context).textTheme
         ),
       ),
-      initialRoute: SplashScreen.id,
+      initialRoute: HomeScreen.id,
       routes: {
         SplashScreen.id: (context) => const SplashScreen(),
         WelcomeScreen.id: (context) => const WelcomeScreen(),
         HomeScreen.id:(context)=>const HomeScreen(),
         Course.id:(context)=>const Course(),
-        Carousel.id:(context)=>const Carousel()
+        OprScreen.id:(context)=>const OprScreen(),
+        Carousel.id:(context)=>const Carousel(),
       },
     );
   }
